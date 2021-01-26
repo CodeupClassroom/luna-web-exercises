a1b2cb744c8d478bf018c8cbd87601c719200e23
David Stephens <stephens.a.david@gmail.com>
2021-01-26 09:19:19 -0600

    completed the morning warmup for january 26

--- js/morning-pushups.js
+++ js/morning-pushups.js
@@ -15,5 +15,16 @@
         }
     }
 
-    console.log(isFactor(21,3), isFactor(3,2), isFactor(NaN, 5), isFactor(88, 'sunshine'))
+    // console.log(isFactor(21,3), isFactor(3,2), isFactor(NaN, 5), isFactor(88, 'sunshine'))
+
+    function firstLastOfArray(arr) {
+        var bucket = [];
+        bucket.push(arr[0]);
+        bucket.push(arr[arr.length - 1]);
+        return bucket;
+    }
+
+    console.log(firstLastOfArray([1,2,3,4]));
+    console.log(firstLastOfArray(["dog", "cat", "rat", "bat"]));
+    console.log(["Geoff"]);
 })();
