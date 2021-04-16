b5ef09e902fc1c155d3758ae4de9a2eaa50d14ea
Sam Lansford <sam.j.lansford@gmail.com>
2021-04-16 14:10:36 -0500

    doing fizzbuzz

--- js/warmup.js
+++ js/warmup.js
@@ -1,6 +1,10 @@
 "use strict"
 
 function fizzbuzz(num) {
-    for (let i = 0; i < num.length; i++) {
+    for (var i=1; i < 101; i++){
+        if (i % 15 === 0) console.log("FizzBuzz");
+        else if (i % 3 === 0) console.log("Fizz");
+        else if (i % 5 === 0) console.log("Buzz");
+        else console.log(i);
     }
 }
